2a72effaad09c712792ed3fe956e32ae0805b57a
Sreejith Markkasseri <sreejith.markkasseri@in.bosch.com>
2024-07-25 00:45:08 +0530

    Bug fixes
    Bug fixes in train script
    re-organized the config files
    added ignore file

--- transformers_explained/utils/data_loader.py
+++ transformers_explained/utils/data_loader.py
@@ -1,32 +1,33 @@
 """Data loader Module."""
 
 from typing import Tuple
-from ast import Tuple
-from torchvision import transforms, datasets
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
 from torch.utils.data import DataLoader
 
-def get_data_loaders(data_set_name: str, img_size: int, batch_size: int) -> Tuple[DataLoader, DataLoader]:
-    """Get Data loaders for the specified dataset.
-    
-    Args:
-        data_set_name: Name of the dataset
-        img_size: Size of the image
-        bath_size: Batcg Size
-    
-    Returns:
-        Train and test Data loaders
-    """
+def get_data_loaders(dataset_name: str, img_size: int, batch_size: int) -> Tuple[DataLoader, DataLoader]:
+    """Get data loaders for the specified dataset.
 
-    transform = transforms.Compose([transforms.Resize((img_size, img_size), 
-                                                      transforms.ToTensor(),
-                                                      transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225],
-                                                                        ))])
-    if data_set_name == 'CIFAR10':
+    Args:
+        dataset_name: Name of the dataset (e.g., "CIFAR10").
+        img_size: Size to resize the images to.
+        batch_size: Batch size for the data loaders.
+
+    Returns:
+        Train and test data loaders.
+    """
+    transform = transforms.Compose([
+        transforms.Resize((img_size, img_size)),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    if dataset_name == 'CIFAR10':
         train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
         test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
     else:
-        raise ValueError(f"Unknown Dataset{data_set_name}")
+        raise ValueError(f"Unknown dataset: {dataset_name}")
+
     train_loader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True)
     test_loader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False)
     return train_loader, test_loader
-
